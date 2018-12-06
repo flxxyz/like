bdb688cb25d663e3ed3ed9406eb4d586f72ee372
flxxyz <i@mdzz.name>
2018-12-06 23:55:35 +0800

    整理入口文件接口，添加简短注释

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,22 +3,19 @@
 //引入自动加载文件
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('DS', DIRECTORY_SEPARATOR, true);
-define('BASE_DIR', realpath(__DIR__ . '/..').DS, true);
-define('APP_DIR', realpath(BASE_DIR.'app').DS, true);
-define('VIEW_DIR', realpath(APP_DIR.'Views').DS, true);
-define('MODEL_DIR', realpath(APP_DIR.'Models').DS, true);
-define('CONTROLLER_DIR', realpath(APP_DIR.'Controllers').DS, true);
-
 use Col\{
     Route,
     Lib\Config
 };
 
+//设置脚本时区
 ini_set('date.timezone', Config::get('app', 'timezone'));
 
+//实例路由
 Route::make(\request());
 
+//引入路由列表
 require_once BASE_DIR . 'route/web.php';
 
+//路由完成
 Route::end();
--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
+//预定义目录常量
+define('DS', DIRECTORY_SEPARATOR, true);
+define('PUBLIC_DIR', realpath(__DIR__).DS, true);
+define('BASE_DIR', realpath(__DIR__ . '/..').DS, true);
+define('APP_DIR', realpath(BASE_DIR.'app').DS, true);
+define('VIEW_DIR', realpath(APP_DIR.'Views').DS, true);
+define('MODEL_DIR', realpath(APP_DIR.'Models').DS, true);
+define('CONTROLLER_DIR', realpath(APP_DIR.'Controllers').DS, true);
+
+//入口文件
 require_once __DIR__.'/../bootstrap/app.php';
